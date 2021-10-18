79c1caa53d0752d7e20e85b7e023913aa8d9daf1
cccovo <87657254+cccovo@users.noreply.github.com>
2021-10-18 15:27:51 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,20 @@
+var vg_1 = "sea_level.vg.json";
+vegaEmbed("#sea_level", vg_1, {"actions": false}).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "natural_disasters.vg.json";
+vegaEmbed("#natural_disasters", vg_2, {"actions": false}).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "temperature.map.vg.json";
+vegaEmbed("#temp", vg_3, {"actions": false}).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "land_loss.vg.json";
+vegaEmbed("#landloss", vg_4, {"actions": false}).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
